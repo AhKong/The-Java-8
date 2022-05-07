6b23e44b2d76b37a086a8a1a1332c2b0ca121732
ahkong <ahreum0412@naver.com>
2022-05-07 22:13:49 +0900

    init

---
+++ src/main/java/ahkong/com/java8t011/Java8t011Application.java
@@ -0,0 +1,13 @@
+package ahkong.com.java8t011;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8t011Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java8t011Application.class, args);
+    }
+
+}
---
+++ src/test/java/ahkong/com/java8t011/Java8t011ApplicationTests.java
@@ -0,0 +1,13 @@
+package ahkong.com.java8t011;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8t011ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
